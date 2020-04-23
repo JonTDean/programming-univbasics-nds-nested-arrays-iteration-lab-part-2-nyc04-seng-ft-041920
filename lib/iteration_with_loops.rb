80bc1c06e6a5e7261bb6250bdecf535def9f1c1f
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-23 17:18:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,12 +9,25 @@ def merge(array_matrix)
     merge(array_matrix)
   else
     middle = (array_matrix.length / 2) - 1
-    left << array_matrix[0..middle]
-    right << array_matrix[middle..array_matrix.length]
+    left = array_matrix[0...middle]
+    right = array_matrix[middle..array_matrix.length]
+
+    # puts left
+    # puts right
+    # p left, right
+    merge_sort(left, right)
   end
 end
 
 def merge_sort(half1, half2)
-  
-  
+  if half1.empty?
+    half2
+  elsif half.empty?
+    half1
+  elsif half1.first < half2.first
+   [half1.first] + merge(half1[1..half1], half2)
+  else 
+    # The reason I need to use 1..half2.length is because half2 is its own array, so we are adding the first element of half2 to the array we are returning to merge.
+    [half.first] + merge(half1, half2[1..half2.length])
+end
 end
