ed4cb2a889330c85f215350bbbfd15df8adca95a
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-23 17:25:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,30 +4,30 @@ def find_min_in_nested_arrays(src)
   merge(src)
 end
 
-def merge(array_matrix)
-  if array_matrix.length <= 1
-    merge(array_matrix)
+def merge_sort(array)
+  if array.length <= 1
+    merge_sort(array)
   else
-    middle = (array_matrix.length / 2) - 1
-    left = array_matrix[0...middle]
-    right = array_matrix[middle..array_matrix.length]
+    middle = (array.length / 2) - 1
+    left = array[0...middle]
+    right = array[middle..array.length]
 
     # puts left
     # puts right
     # p left, right
-    merge_sort(left, right)
+    merge(left, right)
   end
 end
 
-def merge_sort(half1, half2)
+def merge(half1, half2)
   if half1.empty?
     half2
   elsif half2.empty?
     half1
   elsif half1.first < half2.first
-   [half1.first] + merge(half1[1..half1], half2)
+   [half1.first] + merge(half1[1..half1.length], half2)
   else 
     # The reason I need to use 1..half2.length is because half2 is its own array, so we are adding the first element of half2 to the array we are returning to merge.
     [half2.first] + merge(half1, half2[1..half2.length])
-end
+  end
 end
